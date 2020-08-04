385a052998eb1ea3939e1301fb2cb91db3154387
pattonw <pattonw@hhmi.org>
2020-08-04 11:53:54 -0400

    add decimated function versions to __init__.py

--- maximin/__init__.py
+++ maximin/__init__.py
@@ -1,1 +1,7 @@
-from .maximin import maximin_tree_edges, maximin_tree_query, maximin_tree_query_hd
+from .maximin import (
+    maximin_tree_edges,
+    maximin_tree_query,
+    maximin_tree_query_hd,
+    maximin_tree_query_plus_decimated,
+    maximin_tree_query_hd_plus_decimated,
+)
